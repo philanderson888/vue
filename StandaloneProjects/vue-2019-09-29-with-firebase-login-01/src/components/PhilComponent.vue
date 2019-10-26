6275bc8d205a6b965141b36fa3ec1cc51a9f419a
philanderson888 <philanderson888@hotmail.com>
2019-10-26 12:16:01 +0100

    Rearranging projects

--- StandaloneProjects/vue-2019-09-29-with-firebase-login-01/src/components/PhilComponent.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Phil's Component</h1>
-  </div>
-</template>
-<script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-export default class PhilComponent extends Vue {}
-</script>
--- StandaloneProjects/vue-2019-10-05-firebase/src/components/Component02.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-      <hr />
-    <p><strong>Component02</strong></p>
-  </div>
-</template>
