ea7f04515251f349316488e95a844bb5ec9aad15
philanderson888 <philanderson888@hotmail.com>
2020-05-02 14:06:40 +0100

    It was a struggle but got Bulma CSS working

--- StandaloneProjects/vue-authentication-okta-03/src/components/MessageList.vue
+++ StandaloneProjects/vue-authentication-okta-03/src/components/MessageList.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>Hello</p>
+    <ul class="ul-posts" v-if="posts && posts.length">
+      <li class="posts" v-for="post in posts" :key='post.title'>
+        <p><strong>{{post.title}}</strong></p>
+        <p>{{post.body}}</p>
+      </li>
+  </ul>
   </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios'
 @Component
 export default class MessageList extends Vue {
-@Prop() private msg!: string;
+  @Prop() private msg!: string;
+  data(){
+    return{
+      posts:[
+        {
+          title:"title",
+          body:"post body"
+        },
+        {
+                    title:"title",
+          body:"post body"
+        }
+      ]
+    }
+  }
 }
 </script> <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss"
