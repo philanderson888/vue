537163862cf21a466f85d0d881396bfc9664afe6
philanderson888 <philanderson888@hotmail.com>
2020-05-10 16:31:53 +0100

    Firebase_Auth_01 - User Registration Works

---
+++ StandaloneProjects/vue-authentication-firebase-01/src/views/Register.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>Register</h1>
+        <div v-if="error" class="error">{{error.message}}</div>
+        <form @submit.prevent="pressed">
+            <div class="email">
+                <input type="email" v-model="email" placeholder="email" />
+            </div>
+            <div class="password">
+                <input type="password" v-model="password" placeholder="password"/>
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+
+export default {
+    methods:{
+        async pressed(){
+            try{
+                const user = firebase.auth().createUserWithEmailAndPassword(this.email,this.password)
+                console.log()
+                console.group(`user is`)
+                console.log(user)
+                console.groupEnd()
+                console.log()
+                this.$router.replace({name: 'Secret'})
+            }
+            catch(err){
+                console.log(err)
+            }
+        }
+    },
+    data(){
+        return{
+            email:null,
+            password:null,
+            error:null
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
